66f7aab048145db68f943b58d994c98d4e7c7b0e
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-02-16 07:41:50 -0600

    Recreating the tutorial example

---
+++ OOP/E1/OOP.FirstConsoleExercice/AudioManager/Vehicle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    
+    public class Vehicle
+    {
+        public virtual void VehicleSound()
+        {
+            Console.WriteLine("I´m the super class");
+        }
+    }
+}
