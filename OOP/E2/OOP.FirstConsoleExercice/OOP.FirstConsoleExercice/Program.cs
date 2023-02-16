a71f53b37d5da4516085fb145cfacb780c238202
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-02-16 11:09:54 -0600

    Excersice 2
    This program loops only 10 times. Only performances where a sound is played are taken into account.

---
+++ OOP/E2/OOP.FirstConsoleExercice/OOP.FirstConsoleExercice/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using AudioManager;
+
+namespace OOP.FirstConsoleExercice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            while (contador<=10)
+            {
+                Console.WriteLine("Type the vehicle");
+                string vehicleType = Console.ReadLine().ToLower();
+                
+
+
+                Vehicle vehicle=null;
+                switch (vehicleType)
+                {
+                    case "car":
+                        vehicle= new Car();
+                        break;
+
+                    case "truck":
+                        vehicle = new Truck();
+                        break;
+
+                    case "train":
+                       vehicle= new Train();
+                        break;
+
+                    default:
+                        Console.WriteLine("Vehicle not found");
+                        System.Threading.Thread.Sleep(2000);
+                        break;
+                }
+
+                if (vehicle != null)
+                {
+                    vehicle.VehicleSound();
+                    contador++;
+
+                }
+            }
+        }
+    }
+}
