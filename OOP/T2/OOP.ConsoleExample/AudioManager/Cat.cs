f2262fcb38ba0a40bf4e157d6a795e7a1fe95b6a
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-02-15 23:04:55 -0600

    Example made in the Anstraction and polymorphism tutorial

---
+++ OOP/T2/OOP.ConsoleExample/AudioManager/Cat.cs
@@ -0,0 +1,27 @@
+﻿using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AudioManager
+{
+    public class Cat : Animal
+    {
+        public override void animalSound()
+        {
+            AudioFileReader audioFile = new AudioFileReader("Resources/POO_AnimalSounds_cat.wav");
+            WaveOutEvent waveOutEvent = new WaveOutEvent();
+
+            waveOutEvent.Init(audioFile);
+            waveOutEvent.Play();
+            while (waveOutEvent.PlaybackState == PlaybackState.Playing)
+            {
+                System.Threading.Thread.Sleep(500);
+            }
+            waveOutEvent.Dispose();
+        }
+
+    }
+}
