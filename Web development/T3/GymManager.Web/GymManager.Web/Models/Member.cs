4f7f71ce0a95a97f7393ff3e7bf6ebd9bd57319f
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-02-20 09:16:00 -0600

    Progress

---
+++ Web development/T3/GymManager.Web/GymManager.Web/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GymManager.Web.Models
+{
+    public class Member
+    {
+        public string Name { get; set; }   
+        public string LastName { get; set; }
+        public DateTime BirthDay { get; set; }
+        public int CytiId { get; set; }
+        public string Email { get; set; }
+        public bool AllowNesLetter { get; set; }
+
+
+    }
+}
