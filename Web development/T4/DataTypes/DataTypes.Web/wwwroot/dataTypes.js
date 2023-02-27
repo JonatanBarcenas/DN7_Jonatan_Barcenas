890515a1e47954d997e957fde9f2ac1bc449b6c5
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-02-26 20:42:15 -0600

    Web development tutorials

---
+++ Web development/T4/DataTypes/DataTypes.Web/wwwroot/dataTypes.js
@@ -0,0 +1,25 @@
+﻿var element = document.getElementById("example");
+var textExample = "This is a text";
+var aNumber = 10.333;
+
+
+
+var complexObject = {
+    name: "Israel",
+    lastName: "Perez",
+    age: 35
+};
+
+var text1 = "text";
+var u1 = "<u1>";
+
+for (var i = 0; i < textExample.length; i++) {
+    u1 += ("<li>"+textExample[i]+"</li>");
+}
+
+u1 += "</u1>";
+
+var data = "133333";
+
+
+element.innerHTML = typeof(parseInt(data));
---
+++ Web development/T5/DataTypes/DataTypes/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace DataTypes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string text = "This is a string";
+			int age = 19;
+			DateTime now = DateTime.Now;
+			double amount = 0;
+
+			string string2 = text + " a second string " + now;
+
+			string string3 = string.Format("The age is {0}, the time is {1}, and the amount is {2}", age, now, amount);
+
+			String string4 = $"The age is {age}, the time is {now}, and the amount is {amount}";
+
+			char sampleChar = 'C';
+			char[] arrayChar = string4.ToCharArray();
+
+			for (int i = 0; i < arrayChar.Length; i++)
+			{
+				Console.WriteLine(string4[i]);
+			}
+
+			amount = (double)10 / (double)3;
+
+			DateTime dateTime = new DateTime(2000, 1, 1);
+			var timeStamp = now - dateTime;
+
+
+			Console.WriteLine(timeStamp.Days);
+
+			string test = "15";
+
+			string booleanValue = "true";
+			bool isTrue = bool.Parse(booleanValue);
+			age = int.Parse(test);
+
+			Console.ReadKey();
+		}
+	}
+}
