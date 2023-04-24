70b4873d572f51c555abff823a0f10f0aef99c42
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-04-24 09:23:13 -0600

    my new progress

---
+++ 06_ASPNetCoreMVCwEntityFramework/E1/DataAccess.Excersice/DataAccess.Excersice/Entities/LastMembersResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Excersice.Entities
+{
+	public class LastMembersResult
+	{
+		public int MemberId { get; set; }
+		public string InscriptionDate { get; set; }
+		public int MembershipType { get; set; }
+	}
+}
---
+++ 06_ASPNetCoreMVCwEntityFramework/E1/DataAccess.Excersice/DataAccess.Excersice/Entities/SellModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Excersice.Entities
+{
+	public class SellModel
+	{
+		public int ProductId { get; set; }
+		public int UserId { get; set; }
+	}
+}
