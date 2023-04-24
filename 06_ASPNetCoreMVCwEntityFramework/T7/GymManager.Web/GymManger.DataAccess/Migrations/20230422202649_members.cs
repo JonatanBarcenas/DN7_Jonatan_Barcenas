a21acb5221bfd79e091f09d3b88479bb7d5b89ae
JonatanBarcenas <L21650344@zitacuaro.tecnm.mx>
2023-04-24 09:25:18 -0600

    Tutorials progress

---
+++ 06_ASPNetCoreMVCwEntityFramework/T7/GymManager.Web/GymManger.DataAccess/Migrations/20230422202649_members.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GymManger.DataAccess.Migrations
+{
+    public partial class members : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
